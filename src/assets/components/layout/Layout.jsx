eb10eef99a4bad2a0bad71814aaa5b6dcee514f2
maurobrizio <briziomauro2003@gmail.com>
2024-08-19 16:18:58 -0300

    feat: routes done

--- src/assets/components/layout/Layout.jsx
+++ src/assets/components/layout/Layout.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Navbar from '../navbar/Navbar'
 import Footer from '../footer/Footer'
 
-const Layout = () => {
+const Layout = ({children}) => {
     return (
         <div className="flex flex-col min-h-screen">
             <Navbar />
