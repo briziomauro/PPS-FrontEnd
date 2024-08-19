b988218c2673418d6f426611d7315854e8b27a82
maurobrizio <briziomauro2003@gmail.com>
2024-08-19 16:10:52 -0300

    feat: layout

---
+++ src/assets/components/layout/LayoutLogged.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import NavbarLogged from '../navbar/NavbarLogged'
+import Footer from '../footer/Footer'
+
+const LayoutLogged = () => {
+    return (
+        <div className="flex flex-col min-h-screen">
+            <NavbarLogged />
+            <main className="flex-grow">{children}</main>
+            <Footer />
+        </div>
+    )
+}
+
+export default LayoutLogged
---
+++ src/assets/components/navbar/NavbarLogged.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NavbarLogged = () => {
+  return (
+    <div>NavbarLogged</div>
+  )
+}
+
+export default NavbarLogged
