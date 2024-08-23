417780d6e1bc77ee60991cd00b9dd5907e0ea52d
Nicolas Arrastia <nicolasarrastia04@gmail.com>
2024-08-23 19:07:41 -0300

    style: navbar new effect

--- src/components/mainPage/MainPage.jsx
+++ src/components/mainPage/MainPage.jsx
@@ -1,27 +1,38 @@
-import React, { useState } from 'react'
-import 'bootstrap-icons/font/bootstrap-icons.css';
-import Benefits from '../benefits/Benefits';
-import Memberships from '../memberships/Memberships';
+import React, { useState } from "react";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import Benefits from "../benefits/Benefits";
+import Memberships from "../memberships/Memberships";
 
 const MainPage = () => {
-
   return (
     <>
       <div className="flex justify-center items-center h-[550px]">
-        <div className='flex flex-col justify-center mt-10 gap-y-2 font-bebas font-semibold text-black text-8xl w-[1000px]'>
-          <div className='flex justify-center items-center gap-5'>
+        <div className="flex flex-col justify-center mt-10 gap-y-2 font-bebas font-semibold text-black text-8xl w-[1000px]">
+          <div className="flex justify-center items-center gap-5">
             <p>DISFRUTA</p>
-            <img src="https://images.pexels.com/photos/949131/pexels-photo-949131.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" className='h-[80px] w-[330px] object-cover rounded-full' />
-            <p >ENTRENANDO</p>
+            <img
+              src="https://images.pexels.com/photos/949131/pexels-photo-949131.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              alt=""
+              className="h-[80px] w-[330px] object-cover rounded-full"
+            />
+            <p>ENTRENANDO</p>
           </div>
-          <div className='flex justify-center items-center gap-5'>
+          <div className="flex justify-center items-center gap-5">
             <p>ELEVA</p>
             <p>TU</p>
             <p>RUTINA</p>
-            <img src="https://images.pexels.com/photos/841130/pexels-photo-841130.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" className='h-[80px] w-[515px] object-cover rounded-full' />
+            <img
+              src="https://images.pexels.com/photos/841130/pexels-photo-841130.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              alt=""
+              className="h-[80px] w-[515px] object-cover rounded-full"
+            />
           </div>
-          <div className='flex justify-center items-center gap-5'>
-            <img src="https://images.pexels.com/photos/4720796/pexels-photo-4720796.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" className='h-[80px] w-[530px] object-cover rounded-full' />
+          <div className="flex justify-center items-center gap-5">
+            <img
+              src="https://images.pexels.com/photos/4720796/pexels-photo-4720796.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              alt=""
+              className="h-[80px] w-[530px] object-cover rounded-full"
+            />
             <p>MEJORA</p>
             <p>CADA</p>
             <p>DIA</p>
@@ -29,9 +40,9 @@ const MainPage = () => {
         </div>
       </div>
       <Benefits />
-      <Memberships/>
+      <Memberships />
     </>
-  )
-}
+  );
+};
 
-export default MainPage
+export default MainPage;
--- src/components/memberships/Memberships.jsx
+++ src/components/memberships/Memberships.jsx
@@ -1,79 +1,82 @@
-import React from 'react'
-import { FaArrowRightLong } from 'react-icons/fa6'
-import { GoDotFill } from 'react-icons/go'
+import { FaArrowRightLong } from "react-icons/fa6";
+import { GoDotFill } from "react-icons/go";
 
 const Memberships = () => {
-    return (
-        <div className='flex flex-col justify-center items-center text-center mb-10 rounded-xl h-[800px]'>
-            <div className='text-black my-10'>
-                <h2 className='font-bebas text-4xl'>MEMBRESIAS</h2>
-                <p>Mejora tu experiencia entrenando con nuestros planes de membresias</p>
-            </div>
+  return (
+    <div
+      className="flex flex-col justify-center items-center text-center mb-10 rounded-xl h-[800px]"
+      id="Memberships"
+    >
+      <div className="text-black my-10">
+        <h2 className="font-bebas text-4xl">MEMBRESIAS</h2>
+        <p>
+          Mejora tu experiencia entrenando con nuestros planes de membresias
+        </p>
+      </div>
 
-            <div className='flex justify-center gap-28 items-center text-white'>
-                <div className='flex flex-col text-center bg-zinc-800 h-[500px] w-[330px] shadow-lg shadow-black rounded-md hover:scale-105 transition-all duration-200'>
-                    <h3 className='mt-10 text-2xl'>MEMBRESIA 1</h3>
-                    <p className='mt-3 '>Mejor opcion para principiantes</p>
-                    <p className='mt-6 text-4xl'>$25/MES</p>
-                    <ul className='flex flex-col items-center mt-10 text-lg'>
-                        <li className='flex items-center'>
-                            <GoDotFill />
-                            <p>Condicion 1</p>
-                        </li>
-                        <li className='flex items-center'>
-                            <GoDotFill />
-                            <p>Condicion 2</p>
-                        </li>
-                        <li className='flex items-center'>
-                            <GoDotFill />
-                            <p>Condicion 3</p>
-                        </li>
-                        <li className='flex items-center'>
-                            <GoDotFill />
-                            <p>Condicion 4</p>
-                        </li>
-                    </ul>
-                    <div className='flex justify-center mt-10'>
-                        <button className='flex w-[200px] p-3 rounded-full bg-zinc-700 items-center gap-2 justify-center hover:bg-yellow-500 hover:shadow-md hover:shadow-zinc-900 transition-all duration-200'>
-                            <p>EMPEZA AHORA</p>
-                            <FaArrowRightLong className='animate-bounce' />
-                        </button>
-                    </div>
-                </div>
-
-                <div className='flex flex-col text-center bg-zinc-800 h-[500px] w-[330px] shadow-lg shadow-black rounded-md hover:scale-105 transition-all duration-200'>
-                    <h3 className='mt-10 text-2xl'>MEMBRESIA 1</h3>
-                    <p className='mt-3 '>Mejor opcion para principiantes</p>
-                    <p className='mt-6 text-4xl'>$25/MES</p>
-                    <ul className='flex flex-col items-center mt-10 text-lg'>
-                        <li className='flex items-center'>
-                            <GoDotFill />
-                            <p>Condicion 1</p>
-                        </li>
-                        <li className='flex items-center'>
-                            <GoDotFill />
-                            <p>Condicion 2</p>
-                        </li>
-                        <li className='flex items-center'>
-                            <GoDotFill />
-                            <p>Condicion 3</p>
-                        </li>
-                        <li className='flex items-center'>
-                            <GoDotFill />
-                            <p>Condicion 4</p>
-                        </li>
-                    </ul>
-                    <div className='flex justify-center mt-10'>
-                        <button className='flex w-[200px] p-3 rounded-full bg-zinc-700 items-center gap-2 justify-center hover:bg-yellow-500 hover:shadow-md hover:shadow-zinc-900 transition-all duration-200'>
-                            <p>EMPEZA AHORA</p>
-                            <FaArrowRightLong className='animate-bounce' />
-                        </button>
-                    </div>
-                </div>
-
-            </div>
+      <div className="flex justify-center gap-28 items-center text-white">
+        <div className="flex flex-col text-center bg-zinc-800 h-[500px] w-[330px] shadow-lg shadow-black rounded-md hover:scale-105 transition-all duration-200">
+          <h3 className="mt-10 text-2xl">MEMBRESIA 1</h3>
+          <p className="mt-3 ">Mejor opcion para principiantes</p>
+          <p className="mt-6 text-4xl">$25/MES</p>
+          <ul className="flex flex-col items-center mt-10 text-lg">
+            <li className="flex items-center">
+              <GoDotFill />
+              <p>Condicion 1</p>
+            </li>
+            <li className="flex items-center">
+              <GoDotFill />
+              <p>Condicion 2</p>
+            </li>
+            <li className="flex items-center">
+              <GoDotFill />
+              <p>Condicion 3</p>
+            </li>
+            <li className="flex items-center">
+              <GoDotFill />
+              <p>Condicion 4</p>
+            </li>
+          </ul>
+          <div className="flex justify-center mt-10">
+            <button className="flex w-[200px] p-3 rounded-full bg-zinc-700 items-center gap-2 justify-center hover:bg-yellow-500 hover:shadow-md hover:shadow-zinc-900 transition-all duration-200">
+              <p>EMPEZA AHORA</p>
+              <FaArrowRightLong className="animate-bounce" />
+            </button>
+          </div>
         </div>
-    )
-}
 
-export default Memberships
+        <div className="flex flex-col text-center bg-zinc-800 h-[500px] w-[330px] shadow-lg shadow-black rounded-md hover:scale-105 transition-all duration-200">
+          <h3 className="mt-10 text-2xl">MEMBRESIA 1</h3>
+          <p className="mt-3 ">Mejor opcion para principiantes</p>
+          <p className="mt-6 text-4xl">$25/MES</p>
+          <ul className="flex flex-col items-center mt-10 text-lg">
+            <li className="flex items-center">
+              <GoDotFill />
+              <p>Condicion 1</p>
+            </li>
+            <li className="flex items-center">
+              <GoDotFill />
+              <p>Condicion 2</p>
+            </li>
+            <li className="flex items-center">
+              <GoDotFill />
+              <p>Condicion 3</p>
+            </li>
+            <li className="flex items-center">
+              <GoDotFill />
+              <p>Condicion 4</p>
+            </li>
+          </ul>
+          <div className="flex justify-center mt-10">
+            <button className="flex w-[200px] p-3 rounded-full bg-zinc-700 items-center gap-2 justify-center hover:bg-yellow-500 hover:shadow-md hover:shadow-zinc-900 transition-all duration-200">
+              <p>EMPEZA AHORA</p>
+              <FaArrowRightLong className="animate-bounce" />
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Memberships;
