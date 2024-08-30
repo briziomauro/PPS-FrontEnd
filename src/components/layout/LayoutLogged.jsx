8b78494fd3643f65b73ab155690f8c1cac99f7de
maurobrizio <briziomauro2003@gmail.com>
2024-08-30 18:21:27 -0300

    feat: routes added for admin client and professor

--- src/components/layout/LayoutLogged.jsx
+++ src/components/layout/LayoutLogged.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import NavbarLogged from '../navbar/NavbarLogged'
 import Footer from '../footer/Footer'
 
-const LayoutLogged = ({children}) => {
+const LayoutLogged = ({ children }) => {
     return (
         <div className="flex flex-col min-h-screen">
             <NavbarLogged />
