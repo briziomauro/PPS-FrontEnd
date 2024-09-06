bef8be99f617003f72337fd63a5aafadc7e0bcc1
mateocaranta <mateocaranta@gmail.com>
2024-09-06 19:54:38 -0300

    Feat: 404 not found page

--- src/components/notFound/NotFound.jsx
+++ src/components/notFound/NotFound.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 
 const NotFound = () => {
-  return (
-    <div>NotFound</div>
-  )
+    return (
+        <div className=''>
+            <div className='flex relative justify-center items-center h-screen'>
+                <img src="./public/img/Notfound.jpg" alt="not-found" />
+            </div>
+            <div className='absolute flex justify-center items-center'>
+                <p>ERROR 404</p>
+                <p>NOT FOUND!</p>
+            </div>
+        </div>
+    )
 }
 
 export default NotFound
