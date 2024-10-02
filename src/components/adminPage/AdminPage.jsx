6c90b96066ae66ce01b1c9751a57a487a3ec72aa
maurobrizio <briziomauro2003@gmail.com>
2024-10-02 19:15:41 -0300

    feat: new main admin page

--- src/components/adminPage/AdminPage.jsx
+++ src/components/adminPage/AdminPage.jsx
@@ -1,182 +1,210 @@
 import React from 'react'
 import { DiAndroid, DiVim } from 'react-icons/di'
+import { FaArrowRight } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+import { Bar, BarChart, CartesianGrid, Cell, Legend, Line, LineChart, Pie, PieChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts';
+
+const userLocations = [
+  {
+    sedeName: 'Sede Centro',
+    userQuantity: 500,
+    color: "#0f172a"
+  },
+  {
+    sedeName: 'Sede Norte',
+    userQuantity: 320,
+    color: "#1d4ed8"
+  },
+  {
+    sedeName: 'Sede Sur',
+    userQuantity: 450,
+    color: "#16a34a"
+  },
+  {
+    sedeName: 'Sede Oeste',
+    userQuantity: 280,
+    color: "#052e16"
+  },
+  {
+    sedeName: 'Sede Fisherton',
+    userQuantity: 150,
+    color: "#eab308"
+  },
+  {
+    sedeName: 'Sede Pichincha',
+    userQuantity: 380,
+    color: "#dc2626"
+  },
+  {
+    sedeName: 'Sede Puerto Norte',
+    userQuantity: 220,
+    color: "#67e8f9"
+  },
+];
+
+
+const datosMembresias = [
+  { tipo: "Básica", cantidad: 120, color: "#eab308" },
+  { tipo: "Premium", cantidad: 80, color: "#c2410c" },
+];
+
+const nuevosUsuarios = [
+  { mes: 'Enero', cantidad: 30 },
+  { mes: 'Febrero', cantidad: 45 },
+  { mes: 'Marzo', cantidad: 50 },
+  { mes: 'Abril', cantidad: 70 },
+  { mes: 'Mayo', cantidad: 60 },
+  { mes: 'Junio', cantidad: 90 },
+  { mes: 'Julio', cantidad: 100 },
+  { mes: 'Agosto', cantidad: 80 },
+  { mes: 'Septiembre', cantidad: 75 },
+  { mes: 'Octubre', cantidad: 85 },
+  { mes: 'Noviembre', cantidad: 95 },
+  { mes: 'Diciembre', cantidad: 120 },
+];
 
 const AdminPage = () => {
   return (
-    <div className='flex h-screen'>
-      {/* Reporte de asistencias */}
-      <div className='flex-1 flex flex-col h-[800px] justify-evenly items-center'>
-        <h2 className='text-black text-5xl font-bebas'>REPORTES TOTALES</h2>
-        <div>
-          <div className='flex justify-center items-center text-black text-2xl font-bebas'>
+    <div className='flex flex-col min-h-screen'>
+
+      <header className="bg-zinc-700 pl-20 py-4 font-bebas tracking-wider ">
+        <h1 className="text-white text-4xl">
+          Bienvenido <strong className="text-yellow-400">Jhon Doe</strong>
+        </h1>
+      </header>
+
+      <div className='flex justify-center items-center text-black text-5xl font-bebas my-10'>
+        <h3>REPORTES GENERALES</h3>
+      </div>
+
+      <div className='flex'>
+        <div className='flex flex-col flex-1 justify-center items-center mb-4'>
+          <div className='flex justify-center items-center text-black text-3xl font-bebas'>
             <h3>CANTIDAD DE PERSONAS POR SEDE</h3>
           </div>
-          <div className='flex justify-center items-center gap-5 text-white'>
-            <div className=' bg-zinc-800 w-[250px] h-[250px] flex flex-col justify-evenly items-center'>
-              <h4>Sede Rioja</h4>
-              <span className='flex'>
-                <p className='text-8xl'>80</p>
-                <p className='text-sm flex items-end pb-3'>Usuarios</p>
-              </span>
-            </div>
-            <div className=' bg-zinc-800 w-[250px] h-[250px] flex flex-col justify-evenly items-center'>
-              <h4>Sede Abasto</h4>
-              <span className='flex'>
-                <p className='text-8xl'>35</p>
-                <p className='text-sm flex items-end pb-3'>Usuarios</p>
-              </span>
-            </div>
-            <div className=' bg-zinc-800 w-[250px] h-[250px] flex flex-col justify-evenly items-center'>
-              <h4>Sede Pichincha</h4>
-              <span className='flex'>
-                <p className='text-8xl'>109</p>
-                <p className='text-sm flex items-end pb-3'>Usuarios</p>
-              </span>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div className='flex-1 flex flex-col h-[800px] justify-center items-center gap-6'>
-        {/* Valoración de planes nutricionales */}
-        <div>
-          <div className='flex justify-center items-center text-black text-3xl font-bebas'>
-            <h3>VALORACIÓN DE PLANES NUTRICIONALES</h3>
-          </div>
-          <div className='flex flex-col justify-center items-center gap-5 text-white'>
-            <div className=' bg-zinc-800 w-[450px] h-[100px] flex justify-evenly items-center '>
-              <div className='flex flex-col'>
-                <h4>Plan 1</h4>
-                <p>Profesor:</p>
-              </div>
-
-              <span className='flex'>
-                <div className="rating">
-                  <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
-                  <input
-                    type="radio"
-                    name="rating-2"
-                    className="mask mask-star-2 bg-orange-400"
-                    defaultChecked />
-                  <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
-                  <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
-                  <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
-                </div>
-              </span>
-            </div>
-            <div className=' bg-zinc-800 w-[450px] h-[100px] flex justify-evenly items-center '>
-              <div className='flex flex-col'>
-                <h4>Plan 2</h4>
-                <p>Profesor:</p>
-              </div>
-
-              <span className='flex'>
-                <div className="rating">
-                  <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
-                  <input
-                    type="radio"
-                    name="rating-2"
-                    className="mask mask-star-2 bg-orange-400"
-                    defaultChecked />
-                  <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
-                  <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
-                  <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
-                </div>
-              </span>
-            </div>
-            <div className=' bg-zinc-800 w-[450px] h-[100px] flex justify-evenly items-center '>
-              <div className='flex flex-col'>
-                <h4>Plan 3</h4>
-                <p>Profesor:</p>
-              </div>
-
-              <span className='flex'>
-                <div className="rating">
-                  <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
-                  <input
-                    type="radio"
-                    name="rating-2"
-                    className="mask mask-star-2 bg-orange-400"
-                    defaultChecked />
-                  <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
-                  <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
-                  <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
-                </div>
-              </span>
-            </div>
-          </div>
+          <ResponsiveContainer width={600} height={700}>
+            <BarChart
+              width={200}
+              height={300}
+              data={userLocations}
+              margin={{
+                top: 20,
+                right: 30,
+                left: 20,
+                bottom: 5,
+              }}
+            >
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="sedeName" />
+              <YAxis />
+              <Tooltip />
+              <Legend />
+              <Bar dataKey="userQuantity">
+                {userLocations.map((entry, index) => (
+                  <Cell key={`cell-${index}`} fill={entry.color} />
+                ))}
+              </Bar>
+            </BarChart>
+          </ResponsiveContainer>
         </div>
 
-        {/*Valoración de rutinas*/}
-        <div>
-          <div className='flex justify-center items-center text-black text-3xl font-bebas '>
-            <h3>VALORACIÓN DE RUTINAS</h3>
-          </div>
-          <div className='flex flex-col justify-center items-center gap-5 text-white'>
-            <div className=' bg-zinc-800 w-[450px] h-[100px] flex justify-evenly items-center '>
-              <div className='flex flex-col'>
-                <h4>Rutina 1</h4>
-                <p>Profesor:</p>
-              </div>
-
-              <span className='flex'>
-                <div className="rating">
-                  <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
-                  <input
-                    type="radio"
-                    name="rating-2"
-                    className="mask mask-star-2 bg-orange-400"
-                    defaultChecked />
-                  <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
-                  <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
-                  <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
-                </div>
-              </span>
+        <div className='flex flex-col gap-5 flex-1'>
+          <article className="w-3/4">
+            <div className="bg-amber-400 flex items-center justify-between p-2">
+              <h2 className="text-3xl  font-bebas text-white">Centro de Usuarios</h2>
             </div>
-            <div className=' bg-zinc-800 w-[450px] h-[100px] flex justify-evenly items-center '>
-              <div className='flex flex-col'>
-                <h4>Rutina 2</h4>
-                <p>Profesor:</p>
-              </div>
 
-              <span className='flex'>
-                <div className="rating">
-                  <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
-                  <input
-                    type="radio"
-                    name="rating-2"
-                    className="mask mask-star-2 bg-orange-400"
-                    defaultChecked />
-                  <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
-                  <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
-                  <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
-                </div>
-              </span>
+            <ul>
+              <li>
+                <Link
+                  className="bg-zinc-800 flex justify-center items-center gap-3 text-white p-10 text-xl rounded-b-3xl text-center hover:bg-zinc-900 transition-all duration-200"
+                  to="/adminid/user-center"
+                >
+                  Ver usuarios <FaArrowRight className="animate-bounce" />
+                </Link>
+              </li>
+            </ul>
+          </article>
+
+          <article className="w-3/4">
+            <div className="bg-amber-400 flex items-center justify-between p-2">
+              <h2 className="text-3xl  font-bebas text-white">Asignar Turnos</h2>
             </div>
-            <div className=' bg-zinc-800 w-[450px] h-[100px] flex justify-evenly items-center '>
-              <div className='flex flex-col'>
-                <h4>Rutina 3</h4>
-                <p>Profesor:</p>
-              </div>
 
-              <span className='flex'>
-                <div className="rating">
-                  <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
-                  <input
-                    type="radio"
-                    name="rating-2"
-                    className="mask mask-star-2 bg-orange-400"
-                    defaultChecked />
-                  <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
-                  <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
-                  <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
-                </div>
-              </span>
+            <ul>
+              <li>
+                <Link
+                  className="bg-zinc-800 flex justify-center items-center gap-3 text-white p-10 text-xl rounded-b-3xl text-center hover:bg-zinc-900 transition-all duration-200"
+                  to="/adminid/assing-shift"
+                >
+                  Asignar turnos disponibles <FaArrowRight className="animate-bounce" />
+                </Link>
+              </li>
+            </ul>
+          </article>
+
+          <div className='flex gap-5'>
+            <div>
+              <h3 className='text-3xl font-bebas text-black mt-3 mb-5'>MEMBRESIAS MAS POPUALRES</h3>
+              <div>
+                <ResponsiveContainer width={300} height={300}>
+                  <PieChart>
+                    <Tooltip />
+                    <Pie
+                      data={datosMembresias}
+                      dataKey="cantidad"
+                      nameKey="tipo"
+                      cx="50%"
+                      cy="50%"
+                      outerRadius={80}
+                      fill="#8884d8"
+                      label={({ name }) => name}
+                    >
+                      {datosMembresias.map((entry, index) => (
+                        <Cell key={`cell-${index}`} fill={entry.color} />
+                      ))}
+                    </Pie>
+                  </PieChart>
+                </ResponsiveContainer>
+              </div>
+            </div>
+            <div>
+              <div className='flex flex-col justify-center items-center'>
+                <h3 className='text-black text-3xl font-bebas mb-5 mt-3 uppercase'>Nuevos Usuarios por Mes</h3>
+                <ResponsiveContainer width={500} height={300}>
+                  <LineChart data={nuevosUsuarios}>
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="mes" />
+                    <YAxis />
+                    <Tooltip
+                      content={({ payload }) => {
+                        if (payload && payload.length > 0) {
+                          return (
+                            <div className="bg-white p-2 border rounded">
+                              <p>{`Mes: ${payload[0].payload.mes}`}</p>
+                              <p>{`Cantidad: ${payload[0].value}`}</p>
+                            </div>
+                          );
+                        }
+                        return null;
+                      }}
+                    />
+                    <Line
+                      type="monotone"
+                      dataKey="cantidad"
+                      stroke="#dc2626"
+                      strokeWidth={2}
+                      dot
+                    />
+                  </LineChart>
+                </ResponsiveContainer>
+              </div>
             </div>
           </div>
         </div>
 
       </div>
+
     </div>
   )
 }
--- src/components/notFound/NotFound.jsx
+++ src/components/notFound/NotFound.jsx
@@ -1,17 +1,34 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import Footer from '../footer/Footer'
 
 const NotFound = () => {
     return (
-        <div className=''>
-            <div className='flex relative justify-center items-center h-screen'>
-                <img src="/img/Notfound.jpg" alt="not-found" />
+        <>
+            <header className='w-full bg-black h-20 flex justify-center items-center'>
+                <Link to='/'>
+                    <img src="/img/LogoLight.png" alt="" className='h-[40px] w-[120px]' />
+                </Link>
+            </header>
+
+            <div className='h-screen flex flex-col justify-between items-center'>
+                <div className='flex flex-col flex-grow justify-center items-center'>
+                    <div className='flex flex-col gap-3 justify-center text-black items-center font-bebas text-5xl mb-5'>
+                        <p>ERROR 404</p>
+                        <p>NOT FOUND!</p>
+                    </div>
+
+                    <Link to="/" className='bg-zinc-900 text-white font-bebas text-4xl px-5 py-3 rounded-lg'>
+                        <button>VOLVER A INICIO</button>
+                    </Link>
+                </div>
+
+                <Footer />
             </div>
-            <div className='absolute flex justify-center items-center'>
-                <p>ERROR 404</p>
-                <p>NOT FOUND!</p>
-            </div>
-        </div>
+
+        </>
     )
+
 }
 
 export default NotFound
