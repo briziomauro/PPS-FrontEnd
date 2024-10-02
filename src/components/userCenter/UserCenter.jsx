a0bb3687aafd1ed321afdd4ecbe08e8d3ba4af81
maurobrizio <briziomauro2003@gmail.com>
2024-10-02 19:45:26 -0300

    feat: new admin client and shifts pages

--- src/components/userCenter/UserCenter.jsx
+++ src/components/userCenter/UserCenter.jsx
@@ -4,18 +4,18 @@ import UserCenterClient from '../userCenterClient/UserCenterClient'
 
 const UserCenter = () => {
     return (
-        <div className="min-h-screen">
-            <div className="text-gray-800 text-5xl font-bold flex justify-center items-center my-8">
-                <h3>CENTRO DE USUARIOS</h3>
+        <div className="min-h-screen bg-gradient-to-tr from-black via-zinc-900 to-black">
+            <div className="text-white font-bebas text-5xl font-bold flex justify-center items-center">
+                <h3 className='mt-10'>CENTRO DE USUARIOS</h3>
             </div>
-            <div className="flex justify-evenly items-center flex-wrap h-auto space-y-4 lg:space-y-0 lg:space-x-8">
+            <div className="flex justify-evenly items-center flex-wrap h-auto space-y-4 lg:space-y-0 lg:space-x-8 mt-10">
                 {/* Profesores */}
-                <div className="flex-1 max-w-sm p-4">
+                <div className="flex-1 max-w-sm p-4 flex justify-center items-center ">
                     <UserCenterProfessor />
                 </div>
 
                 {/* Alumnos */}
-                <div className="flex-1 max-w-sm p-4">
+                <div className="flex-1 max-w-sm p-4 flex justify-center items-center">
                     <UserCenterClient />
                 </div>
             </div>
--- src/components/userCenterProfessor/UserCenterProfessor.jsx
+++ src/components/userCenterProfessor/UserCenterProfessor.jsx
@@ -1,23 +1,106 @@
+import { Input } from 'postcss'
 import React from 'react'
 
 const UserCenterProfessor = () => {
     return (
         <div className="ml-10">
-            <p className="text-4xl font-bold text-gray-800 mb-4">Profesores</p>
-            <div className="flex bg-zinc-800 w-[500px] h-[150px] rounded-lg shadow-lg p-4 items-center">
-                <span className="rounded-full bg-white w-[100px] h-[100px] flex-shrink-0"></span>
-                <div className="ml-4 text-sm text-gray-100">
-                    <p className="font-semibold">Nombre Completo: Pepe Rodriguez</p>
-                    <p className="text-gray-400">ID: #12345</p>
-                    <div className="mt-2">
-                        <button className="bg-red-600 text-white rounded-lg w-[100px] h-[30px] mr-2 hover:bg-red-700 transition-all duration-300">
-                            Eliminar
-                        </button>
-                        <button className="bg-blue-600 text-white rounded-lg w-[100px] h-[30px] hover:bg-blue-700 transition-all duration-300">
-                            Editar
-                        </button>
+            <div className='flex justify-evenly items-center mb-4'>
+                <p className="text-4xl text-white uppercase font-bebas">Profesores</p>
+                <input type="text" className='bg-white rounded-full p-2' placeholder="Buscar..." />
+            </div>
+            <div className='overflow-y-scroll h-[600px] flex flex-col gap-5 p-3'>
+                <div className="flex bg-zinc-800 w-[500px] h-[150px] rounded-lg shadow-lg p-4 items-center">
+                    <span className="rounded-full bg-white w-[100px] h-[100px] flex-shrink-0"></span>
+                    <div className="ml-4 text-sm text-gray-100">
+                        <p className="font-semibold">Nombre Completo: Pepe Rodriguez</p>
+                        <p className="text-gray-400">ID: #12345</p>
+                        <div className="mt-2">
+                            <button className="bg-red-600 text-white rounded-lg w-[100px] h-[30px] mr-2 hover:bg-red-700 transition-all duration-300">
+                                Eliminar
+                            </button>
+                            <button className="bg-blue-600 text-white rounded-lg w-[100px] h-[30px] hover:bg-blue-700 transition-all duration-300">
+                                Editar
+                            </button>
+                        </div>
                     </div>
                 </div>
+                <div className="flex bg-zinc-800 w-[500px] h-[150px] rounded-lg shadow-lg p-4 items-center">
+                    <span className="rounded-full bg-white w-[100px] h-[100px] flex-shrink-0"></span>
+                    <div className="ml-4 text-sm text-gray-100">
+                        <p className="font-semibold">Nombre Completo: Pepe Rodriguez</p>
+                        <p className="text-gray-400">ID: #12345</p>
+                        <div className="mt-2">
+                            <button className="bg-red-600 text-white rounded-lg w-[100px] h-[30px] mr-2 hover:bg-red-700 transition-all duration-300">
+                                Eliminar
+                            </button>
+                            <button className="bg-blue-600 text-white rounded-lg w-[100px] h-[30px] hover:bg-blue-700 transition-all duration-300">
+                                Editar
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                <div className="flex bg-zinc-800 w-[500px] h-[150px] rounded-lg shadow-lg p-4 items-center">
+                    <span className="rounded-full bg-white w-[100px] h-[100px] flex-shrink-0"></span>
+                    <div className="ml-4 text-sm text-gray-100">
+                        <p className="font-semibold">Nombre Completo: Pepe Rodriguez</p>
+                        <p className="text-gray-400">ID: #12345</p>
+                        <div className="mt-2">
+                            <button className="bg-red-600 text-white rounded-lg w-[100px] h-[30px] mr-2 hover:bg-red-700 transition-all duration-300">
+                                Eliminar
+                            </button>
+                            <button className="bg-blue-600 text-white rounded-lg w-[100px] h-[30px] hover:bg-blue-700 transition-all duration-300">
+                                Editar
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                <div className="flex bg-zinc-800 w-[500px] h-[150px] rounded-lg shadow-lg p-4 items-center">
+                    <span className="rounded-full bg-white w-[100px] h-[100px] flex-shrink-0"></span>
+                    <div className="ml-4 text-sm text-gray-100">
+                        <p className="font-semibold">Nombre Completo: Pepe Rodriguez</p>
+                        <p className="text-gray-400">ID: #12345</p>
+                        <div className="mt-2">
+                            <button className="bg-red-600 text-white rounded-lg w-[100px] h-[30px] mr-2 hover:bg-red-700 transition-all duration-300">
+                                Eliminar
+                            </button>
+                            <button className="bg-blue-600 text-white rounded-lg w-[100px] h-[30px] hover:bg-blue-700 transition-all duration-300">
+                                Editar
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                <div className="flex bg-zinc-800 w-[500px] h-[150px] rounded-lg shadow-lg p-4 items-center">
+                    <span className="rounded-full bg-white w-[100px] h-[100px] flex-shrink-0"></span>
+                    <div className="ml-4 text-sm text-gray-100">
+                        <p className="font-semibold">Nombre Completo: Pepe Rodriguez</p>
+                        <p className="text-gray-400">ID: #12345</p>
+                        <div className="mt-2">
+                            <button className="bg-red-600 text-white rounded-lg w-[100px] h-[30px] mr-2 hover:bg-red-700 transition-all duration-300">
+                                Eliminar
+                            </button>
+                            <button className="bg-blue-600 text-white rounded-lg w-[100px] h-[30px] hover:bg-blue-700 transition-all duration-300">
+                                Editar
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                <div className="flex bg-zinc-800 w-[500px] h-[150px] rounded-lg shadow-lg p-4 items-center">
+                    <span className="rounded-full bg-white w-[100px] h-[100px] flex-shrink-0"></span>
+                    <div className="ml-4 text-sm text-gray-100">
+                        <p className="font-semibold">Nombre Completo: Pepe Rodriguez</p>
+                        <p className="text-gray-400">ID: #12345</p>
+                        <div className="mt-2">
+                            <button className="bg-red-600 text-white rounded-lg w-[100px] h-[30px] mr-2 hover:bg-red-700 transition-all duration-300">
+                                Eliminar
+                            </button>
+                            <button className="bg-blue-600 text-white rounded-lg w-[100px] h-[30px] hover:bg-blue-700 transition-all duration-300">
+                                Editar
+                            </button>
+                        </div>
+                    </div>
+                </div>
+
+
             </div>
         </div>
     )
