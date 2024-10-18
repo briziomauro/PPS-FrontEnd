ced3fd4626277f03bbb6e282926563e4feccbb52
Nicolas Arrastia <nicolasarrastia04@gmail.com>
2024-10-18 19:42:34 -0300

    feat: style step 2 and 3

--- src/components/memberships/Memberships.jsx
+++ src/components/memberships/Memberships.jsx
@@ -17,9 +17,13 @@ const Memberships = () => {
       <div className="flex justify-center gap-28 items-center text-white mt-10">
         <div className="flex flex-col text-center bg-gradient-to-br from-black via-zinc-800 to-black h-[570px] w-[360px] border border-yellow-300 rounded-md hover:scale-105 transition-all duration-200">
           <h3 className="mt-10 text-3xl font-bebas">ESTANDAR</h3>
-          <p className="mt-3 italic">"La opción ideal para quienes recién comienzan."</p>
-          <div className="flex justify-center items-center h-[1px] bg-yellow-400 w-full mt-4"/>
-          <p className="flex justify-center items-center gap-3 mt-6 text-4xl">$20.000 <span className="text-xl">ARS/MES</span></p>
+          <p className="mt-3 italic">
+            "La opción ideal para quienes recién comienzan."
+          </p>
+          <div className="flex justify-center items-center h-[1px] bg-yellow-400 w-full mt-4" />
+          <p className="flex justify-center items-center gap-3 mt-6 text-4xl">
+            $20.000 <span className="text-xl">ARS/MES</span>
+          </p>
           <ul className="flex flex-col gap-2 items-center mt-10 text-lg w-[340px]">
             <li className="flex items-center">
               <p>- Acceso limitado a servicios generales.</p>
@@ -35,7 +39,7 @@ const Memberships = () => {
             </li>
           </ul>
           <div className="flex justify-center mt-10">
-            <Link to={"/register"}>
+            <Link to={"/register/1"}>
               <div className="flex w-[200px] p-3 rounded-full bg-white items-center text-black gap-2 justify-center hover:bg-yellow-500 hover:text-white hover:shadow-md hover:shadow-zinc-900 transition-all duration-200">
                 <p>EMPIEZA AHORA</p>
                 <FaArrowRightLong className="animate-bounce" />
@@ -46,9 +50,14 @@ const Memberships = () => {
 
         <div className="flex flex-col items-center text-center bg-gradient-to-bl from-black via-zinc-800 to-black h-[570px] w-[360px] border border-yellow-300 rounded-md hover:scale-105 transition-all duration-200">
           <h3 className="mt-10 text-3xl font-bebas">PREMIUM</h3>
-          <p className="mt-3 italic">"Perfecta para usuarios que buscan el máximo rendimiento y beneficios."</p>
-          <div className="flex justify-center items-center h-[1px] bg-yellow-400 w-full mt-4"/>
-          <p className="flex justify-center items-center gap-3 mt-6 text-4xl">$32.000 <span className="text-xl">ARS/MES</span></p>
+          <p className="mt-3 italic">
+            "Perfecta para usuarios que buscan el máximo rendimiento y
+            beneficios."
+          </p>
+          <div className="flex justify-center items-center h-[1px] bg-yellow-400 w-full mt-4" />
+          <p className="flex justify-center items-center gap-3 mt-6 text-4xl">
+            $32.000 <span className="text-xl">ARS/MES</span>
+          </p>
           <ul className="flex flex-col gap-2 items-center mt-10 text-lg w-[340px]">
             <li className="flex items-center">
               <p>- Acceso completo a todos los servicios.</p>
--- src/components/registerPage/Confirmation.jsx
+++ src/components/registerPage/Confirmation.jsx
@@ -1,99 +1,150 @@
-import React, { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Confirmation = ({ lastStep }) => {
-    const [clientStoredData, setClientStoredData] = useState(null);
-    const [clientDni, setClientDni] = useState('')
-    const [error, setError] = useState(null);
-    const [loadingRegister, setLoadingRegister] = useState(false);
+  const [clientStoredData, setClientStoredData] = useState(null);
+  const [clientDni, setClientDni] = useState("");
+  const [error, setError] = useState(null);
+  const [loadingRegister, setLoadingRegister] = useState(false);
+  const [countdown, setCountdown] = useState(5); // Contador de 5 segundos
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        const getStoredClient = async () => {
-            try {
-                const response = await fetch("https://localhost:7179/api/Client/GetStoredClientUserData", {
-                    method: "GET",
-                    headers: {
-                        'Content-Type': 'application/json',
-                    },
-                    credentials: "include",
-                });
+  // Obtener datos del cliente almacenado
+  useEffect(() => {
+    const getStoredClient = async () => {
+      try {
+        const response = await fetch(
+          "https://localhost:7179/api/Client/GetStoredClientUserData",
+          {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            credentials: "include",
+          }
+        );
 
-                if (!response.ok) {
-                    throw new Error("Error al obtener los detalles del cliente");
-                }
+        if (!response.ok) {
+          throw new Error("Error al obtener los detalles del cliente");
+        }
 
-                const data = await response.json();
-                console.log(data);
-                setClientStoredData(data)
-                setClientDni(data.clientRequest.dniclient)
-            } catch (error) {
-                setError(error.message);
-            }
-        };
+        const data = await response.json();
+        setClientStoredData(data);
+        setClientDni(data.clientRequest.dniclient);
+      } catch (error) {
+        setError(error.message);
+      }
+    };
 
-        getStoredClient();
-    }, []);
+    getStoredClient();
+  }, []);
 
-
-    if (error) {
-        return <div className='text-red-500'>Error: {error}</div>;
+  // Temporizador para ejecutar la creación del cliente después de 5 segundos
+  useEffect(() => {
+    if (countdown > 0) {
+      const timer = setTimeout(() => setCountdown(countdown - 1), 1000);
+      return () => clearTimeout(timer);
+    } else {
+      // Ejecutar la creación del cliente cuando el contador llegue a 0
+      handleRegister();
     }
-    console.log(clientStoredData)
+  }, [countdown]);
 
-    const handleRegister = async (e) => {
-        e.preventDefault();
+  const handleRegister = async () => {
+    setLoadingRegister(true);
+    try {
+      const response = await fetch(
+        "https://localhost:7179/api/Client/AddClient",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(clientDni),
+        }
+      );
 
-        const createClientRegister = async () => {
-            try {
-                const response = await fetch('https://localhost:7179/api/Client/AddClient', {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json',
-                    },
-                    body: JSON.stringify(clientDni)
-                });
-
-                if (!response.ok) {
-                    throw new Error('Error al registrar');
-                }
-                navigate("/login")
-            } catch (error) {
-                setError(error.message);
-            } finally {
-                setLoadingRegister(false);
-            }
-        };
-
-        await createClientRegister();
+      if (!response.ok) {
+        throw new Error("Error al registrar");
+      }
+      navigate("/login");
+    } catch (error) {
+      setError(error.message);
+    } finally {
+      setLoadingRegister(false);
     }
+  };
 
-    return (
-        <div className='flex flex-col justify-center text-black w-full'>
-            <h2 className="font-bebas mt-6 text-4xl text-zinc-700 text-center ">
-                CONFIRMACIÓN
-            </h2>
-            <form className='flex flex-col text-center' onSubmit={handleRegister}>
-                <h3>RESUMEN DE DATOS:</h3>
-                <div>
-                    <p>Membresia: <span className='uppercase'>{clientStoredData ? clientStoredData.clientRequest.typememberships : 'Cargando...'}</span></p>
-                </div>
-                <div>
-                    <h3>Datos del Usuario:</h3>
-                    <div>
-                        <p>Hola, {clientStoredData ? clientStoredData.clientRequest.firstname : 'Cargando...'}</p>
-                        <p>DNI: {clientStoredData ? clientStoredData.clientRequest.dniclient : 'Cargando'}</p>
-                    </div>
-                </div>
-                <div className="flex justify-between mt-6 mx-5 gap-3">
-                    <button type='submit' className="bg-yellow-400 rounded-full w-full text-white px-6 py-3 transition-all duration-200 hover:bg-yellow-500 hover:font-bold flex-1">
-                        Confirmar
-                    </button>
-                </div>
-            </form>
+  if (error) {
+    return <div className="text-red-500">Error: {error}</div>;
+  }
+
+  return (
+    <div className="flex flex-col justify-center text-black w-full">
+      <h2 className="font-bebas mt-11 text-5xl text-zinc-700 text-center ">
+        CONFIRMACIÓN
+      </h2>
+      <div className="flex flex-col text-center  text-zinc-700 mt-5">
+        <div>
+          <p className="text-2xl">
+            Membresia:{" "}
+            <span className="uppercase">
+              {clientStoredData
+                ? clientStoredData.clientRequest.typememberships
+                : "Cargando..."}
+            </span>
+          </p>
         </div>
-    );
-}
+        <div className="mt-3">
+          <div>
+            <p className="text-2xl">
+              ¡Bienvenido!{" "}
+              {clientStoredData
+                ? clientStoredData.clientRequest.firstname
+                : "Cargando..."}{" "}
+              {clientStoredData
+                ? clientStoredData.clientRequest.lastname
+                : "Cargando..."}
+            </p>
+          </div>
+        </div>
+
+        {/* Mostrar el contador */}
+        <div className="mt-5 text-center text-xl">
+          {loadingRegister ? (
+            <p className="text-yellow-500">Registrando...</p>
+          ) : (
+            <p>
+              Te redirigiremos en {countdown} segundos
+              {countdown > 0 && (
+                <svg
+                  className="animate-spin ml-2 h-8 w-8 text-yellow-500 inline-block"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                >
+                  <circle
+                    className="opacity-25"
+                    cx="12"
+                    cy="12"
+                    r="10"
+                    stroke="currentColor"
+                    strokeWidth="4"
+                  ></circle>
+                  <path
+                    className="opacity-75"
+                    fill="currentColor"
+                    d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4z"
+                  ></path>
+                </svg>
+              )}
+            </p>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Confirmation;
